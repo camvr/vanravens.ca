a1ae2df7df72bed9ff6e3685ebc0cde45aee718c
Cam Van Ravens <camvanravens@outlook.com>
2021-09-12 11:57:17 -0400

    Initial webpage setup

---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <vue-command
+          :commands="commands"
+          :hide-bar="true"
+          :hide-title="true"
+          :prompt="prompt"
+        />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import VueCommand, { createStdout } from 'vue-command'
+import 'vue-command/dist/vue-command.css'
+
+const PROMPT = 'anon@vanravens.ca:~ $'
+
+export default {
+  components: {
+    VueCommand
+  },
+
+  data: () => ({
+    commands: {
+      'hello-world': () => createStdout('Hello world')
+    },
+
+    // Terminal cursor position
+    cursor: 0,
+    history: [],
+    options: {
+      long: {
+        loading: ['amount', 'timeout']
+      }
+    },
+    prompt: PROMPT,
+    stdin: ''
+  })
+}
+</script>
